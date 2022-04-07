5fc4e8ccf365fbb3140ba4a11ed9a88ceb608a4d
Michael Klein <Michaelklein1997@gmail.com>
2022-04-06 19:08:10 -0500

    finished bubble sort algo

---
+++ BubbleSort.js
@@ -0,0 +1,17 @@
+// The largest value bubbles to the top
+
+const bubbleSort = (arr) => {
+  for (let i = arr.length; i > 0; i--) {
+    // console.log(arr)
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(bubbleSort([37, 45, 29, 8]));
