9fdad4d927064b9b68e46c2b3c58fc41305d6343
Michael Klein <Michaelklein1997@gmail.com>
2022-04-06 19:12:47 -0500

    modified bubble sort algo to finish once dataset is sorted

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,14 +1,21 @@
 // The largest value bubbles to the top
 
 const bubbleSort = (arr) => {
+    let noSwaps;
   for (let i = arr.length; i > 0; i--) {
+      noSwaps = true
     // console.log(arr)
     for (let j = 0; j < i - 1; j++) {
+      let swap = false
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j+1];
         arr[j+1] = temp;
+        noSwaps = false
       }
+    }
+    if (noSwaps) {
+        break
     }
   }
   return arr;
